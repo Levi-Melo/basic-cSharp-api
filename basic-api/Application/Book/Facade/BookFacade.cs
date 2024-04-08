80723893b8bc5fc7b8aeba2559ab860f1f21c7a7
Levi-Melo <levicontat@outlook.com>
2024-04-07 22:48:43 -0300

    Add book controller and facade

---
+++ basic-api/Application/Book/Facade/BookFacade.cs
@@ -0,0 +1,20 @@
+﻿using basic_api.Application.Base;
+using basic_api.Application.Base.UseCase;
+using basic_api.Domain.Book.Facade;
+using basic_api.Infrastructure.Database.Models;
+
+namespace basic_api.Application.Book.Facade
+{
+    public class BookFacade(
+        GetUseCase<BookModel> getUseCase, 
+        DeleteUseCase<BookModel> deleteUseCase, 
+        InsertUseCase<BookModel> insertUseCase, 
+        UpdateUseCase<BookModel> updateUseCase
+        ) : Facade<BookModel>(
+            getUseCase, 
+            deleteUseCase, 
+            insertUseCase, 
+            updateUseCase), IBookFacade
+    {
+    }
+}
---
+++ basic-api/Domain/Book/Facade/BookFacade.cs
@@ -0,0 +1,9 @@
+﻿using basic_api.Domain.Base.Facade;
+using basic_api.Infrastructure.Database.Models;
+
+namespace basic_api.Domain.Book.Facade
+{
+    public interface IBookFacade : IFacade<BookModel>
+    {
+    }
+}
