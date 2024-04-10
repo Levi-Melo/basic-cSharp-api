8ebda5442ddc4a9ccbac3a8069298878298c851f
Levi-Melo <levicontat@outlook.com>
2024-04-09 23:19:44 -0300

    add FileService to upload ebooks on S3

---
+++ basic-api/Data/Services/FileService.cs
@@ -0,0 +1,10 @@
+namespace basic_api.Data.Services
+{
+    public interface IFileService
+    {
+        Task<bool> UploadAsync(string fileName, byte[] buffer);
+
+        Task<byte[]> DownloadAsync(string fileName, byte[] buffer);
+
+    }
+}
