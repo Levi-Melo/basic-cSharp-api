2e8eb60d5fa6a3c49b86cf3fdad9969629148fe1
Levi-Melo <levicontat@outlook.com>
2024-04-13 13:30:01 -0300

    add run of migrations

--- basic-api/Infrastructure/Database/Context/DataContext.cs
+++ basic-api/Infrastructure/Database/Context/DataContext.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using basic_api.Data.Repositories;
+using basic_api.Infrastructure.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace basic_api.Infrastructure.Database.Context
 {
-    public class DataContext : Context
+    public class DataContext(DbContextOptions<DataContext> options) : Context(options)
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            throw new NotImplementedException();
+            AccountRepository.OnModelCreating(modelBuilder);
+            BookRepository.OnModelCreating(modelBuilder);
+            GenreRepository.OnModelCreating(modelBuilder);
+            LocationRepository.OnModelCreating(modelBuilder);
+            OrderRepository.OnModelCreating(modelBuilder);
+            PublisherRepository.OnModelCreating(modelBuilder);
+            RoleRepository.OnModelCreating(modelBuilder);
+            StockRepository.OnModelCreating(modelBuilder);
+            TenantRepository.OnModelCreating(modelBuilder);
+            WriterRepository.OnModelCreating(modelBuilder);
         }
     }
 }
